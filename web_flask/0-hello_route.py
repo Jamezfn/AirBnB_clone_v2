e770f0d330d821c4fcde9b1c7019a11e93b48889
Jamezfn <jamesmakachia19@gmail.com>
2024-07-28 02:09:27 -0700

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
-from flask import Flask, request
-from markupsafe import escape
+"""a script that starts a flask web application"""
+from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def hello_hbnb():
+    """ a function that prints hello `hbnb at the root"""
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
