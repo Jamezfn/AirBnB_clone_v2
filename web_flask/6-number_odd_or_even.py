828c9c77060bdaa4f0a0ec54bee5debc7612ecb6
Jamezfn <jamesmakachia19@gmail.com>
2024-07-28 07:15:48 -0700

    parity

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,7 +49,8 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """ display n only if it is an integer"""
-    return render_template('6-number_odd_or_even.html', n=n)
+    parity = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', n=f'{n} is {parity}')
 
 
 if __name__ == "__main__":
