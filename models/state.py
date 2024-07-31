63a2455a2c7c14127a875c71139035d4cce77103
Jamezfn <jamesmakachia19@gmail.com>
2024-07-31 02:11:58 -0700

    improve engine

--- models/state.py
+++ models/state.py
@@ -21,4 +21,15 @@ class State(BaseModel, Base):
     @property
     def cities(self):
         """Returns the list of cities belonging to this state"""
-        return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
