fa651c851805d68f95828c53a561ee650c5c1adb
Jamezfn <jamesmakachia19@gmail.com>
2025-06-27 12:09:39 +0300

    Bug free

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,29 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
+        """Instantiates a new model"""
+        if kwargs:
+            valid_keys = {'id', 'created_at', 'updated_at', '__class__'}
+            for key, value in kwargs.items():
+                if key not in valid_keys:
+                    raise KeyError(f"Invalid key: {key}")
+                if key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != '__class__':
+                    setattr(self, key, value)
+            self.id = getattr(self, 'id', str(uuid.uuid4()))
+            self.created_at = getattr(self, 'created_at', datetime.now())
+            self.updated_at = datetime.now()
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -29,9 +33,8 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
