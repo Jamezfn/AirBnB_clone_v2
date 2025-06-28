dafa8abad4e0b056d85690a6cc1f18d0b031ee33
Jamezfn <jamesmakachia19@gmail.com>
2025-06-28 06:36:13 +0300

    modified do all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -25,6 +26,7 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
+        os.makedirs(os.path.dirname(FileStorage.__file_path) or '.', exist_ok=True)
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
